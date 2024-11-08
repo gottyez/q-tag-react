5d743277a062e2fbfe8eb15245a41b3b66c033bb
Krid-chaichaloemdet <kridtue01@hotmail.com>
2024-11-08 16:03:10 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <div> tes ttestsetsetsetetstset</div>
+      <div> tes asdasd</div>
       <div>KUY </div>
     </>
   )
