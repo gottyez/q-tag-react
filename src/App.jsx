03f834bbd8928c0bb1b9fa79ad8a143f88e748ca
charin.go <got171143@gmail.com>
2024-11-09 10:53:33 +0700

    add div hello to App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Router from './router/router'
+
+function App() {
+
+  return (
+    <>              
+      {/* <Router/> */}
+      <div>hello</div>
+    </>
+  )
+}
+
+export default App
